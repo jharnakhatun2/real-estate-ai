cbdc394350c283b7a6a4a8ab2bab9c8815adea2a
sohanemon <m.sohanemon@gmail.com>
2023-06-16 23:37:27 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className='text-3xl'>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo earum
+      eaque soluta dicta sapiente odio! Quas, incidunt reprehenderit nam ex nisi
+      consequuntur, iste possimus dolore totam aspernatur hic, ut doloremque.
+    </main>
+  );
+}
+
+export default App;
