29a39d7d3e17e64d158a1e650a111ab4161aabff
sohanemon <m.sohanemon@gmail.com>
2023-06-19 18:38:46 +0600

    in: react icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'indigo',
+      },
+    },
   },
   plugins: [],
 };
