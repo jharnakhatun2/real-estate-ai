14b92ba1d49a1513e8a8053a514c5b206f1db6af
sohanemon <m.sohanemon@gmail.com>
2023-06-19 15:28:36 +0600

    feat: ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <main className='text-3xl'>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo earum
-      eaque soluta dicta sapiente odio! Quas, incidunt reprehenderit nam ex nisi
-      consequuntur, iste possimus dolore totam aspernatur hic, ut doloremque.
-    </main>
-  );
+  return <main></main>;
 }
 
 export default App;
