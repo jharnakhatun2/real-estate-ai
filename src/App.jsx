8002eb83217fb54f6de2a71dde54ee848d2b0596
codernoyan <coder.noyan@gmail.com>
2023-06-20 01:16:59 +0600

    added dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './routes/routes/Routes';
 
 function App() {
   return (
-    <main className='text-3xl'>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo earum
-      eaque soluta dicta sapiente odio! Quas, incidunt reprehenderit nam ex nisi
-      consequuntur, iste possimus dolore totam aspernatur hic, ut doloremque.
-    </main>
+    <RouterProvider router={router}/>
   );
 }
 
