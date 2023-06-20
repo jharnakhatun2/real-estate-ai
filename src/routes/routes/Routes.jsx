2622fbfbe91677bf45302548b1b8f3dc95ea008d
Jharna203 <jharnakhatun203@gmail.com>
2023-06-20 11:15:30 +0600

    added login and signin form style

--- src/routes/routes/Routes.jsx
+++ src/routes/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Dashboard from "../../pages/dashboard/Dashboard";
 import Home from "../../pages/home/Home";
+import Register from "pages/SignUP/Register";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />
+      },
+      {
+        path: '/signIn',
+        element: <Register />
       }
     ]
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [ 
+    './index.html', 
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require('@tailwindcss/forms'),require('tw-elements/dist/plugin.cjs')],
 };
