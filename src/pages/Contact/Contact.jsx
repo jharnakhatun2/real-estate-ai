1d14176ec98ec21860b3edc0fd23dbe1afdfcb9e
atik94 <atikurrahmanatik94@gmail.com>
2023-06-20 13:40:21 +0600

    daisyui setup

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,12 @@
 const Contact = () => {
   return (
     <div>
-      <h1>This is contact page</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
+import Contact from "pages/Contact/Contact";
+
 export default function Home() {
   return (
     <main>
       <h2>this is home</h2>
       <h3>hello</h3>
+      <Contact></Contact>
     </main>
-  )
+  );
 }
