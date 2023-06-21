304ba6bedd7bc3f94e9db0f22c27e858ad4fc4ed
codernoyan <coder.noyan@gmail.com>
2023-06-21 20:53:34 +0600

    added validation

--- src/components/dashboard/propertyForm/Form.jsx
+++ src/components/dashboard/propertyForm/Form.jsx
@@ -4,7 +4,7 @@ export default function Form() {
   const {
     register,
     handleSubmit,
-    // formState: { errors },
+    formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
@@ -12,94 +12,211 @@ export default function Form() {
   };
   
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* location information */}
-      <div>
-        <label htmlFor="location">Location</label>
-        <div>
-          <input type="text" name="street" placeholder="Street Address" {...register('street')} />
-          <input type="text" name="city" placeholder="City" {...register('city')} />
-          <input type="text" name="state" placeholder="State" {...register('state')} />
-          <input type="number" name="zipCode" placeholder="Zip Code" {...register('zipCode')} />
-        </div>
-      </div>
-      {/* property details */}
-      <div>
-        <label htmlFor="location">Property Details</label>
-        <div>
-          <input type="number" name="bedrooms" placeholder="Number of Bedrooms" {...register('bedrooms')} />
-          <input type="number" name="bathrooms" placeholder="Number of Bathrooms" {...register('bathrooms')} />
-          <input type="number" name="squareFootage" placeholder="Square Footage"
-            {...register('squareFootage')} />
-        </div>
-      </div>
-      {/* property condition */}
-      <div>
-        {/* condition */}
-        <div>
-          <label htmlFor="location">Property Condition</label>
-          <div>
-            <select name="condition" {...register('condition')}>
-              <option defaultValue hidden>Select</option>
-              <option value="Excellent">Excellent</option>
-              <option value="Good">Good</option>
-              <option value="Fair">Fair</option>
-              <option value="Poor">Poor</option>
-            </select>
+    <div>
+      <section className="py-1 bg-indigo-50/20">
+        <div className="w-full px-4 mx-auto mt-6">
+          <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+            <div className="rounded-t bg-white mb-0 px-6 py-6">
+              <div className="text-center flex justify-between">
+                <h6 className="text-blueGray-700 text-xl font-bold">
+                  Fill up the forms to generate data
+                </h6>
+              </div>
+            </div>
+            <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                  Location Information
+                </h6>
+                <div className="flex flex-wrap">
+                  <div className="w-full lg:w-6/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Street Address
+                      </label>
+                      <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="street" {...register("street", { required: "street is required" })} />
+                      {errors?.street && <p className="mt-1 text-red-500 font-medium">{errors?.email.message}</p>}
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-6/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                        City
+                      </label>
+                      <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="city" {...register("city", { required: "city is required" })} />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-6/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                        State
+                      </label>
+                      <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="state" {...register("state", { required: "state is required" })} />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-6/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Zip Code
+                      </label>
+                      <input type="number" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="zipcode" {...register("zipcode", { required: "zipcode is required" })} />
+                    </div>
+                  </div>
+                </div>
+                <hr className="mt-6 border-b-1 border-blueGray-300" />
+                <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                  Property Details
+                </h6>
+                <div className="flex flex-wrap">
+                  <div className="w-full lg:w-4/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Number of Bedrooms
+                      </label>
+                      <input type="number" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="bedrooms" {...register("bedrooms", { required: "bedrooms is required" })} />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-4/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Number of Bathrooms
+                      </label>
+                      <input type="number" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="bathrooms" {...register("bathrooms", { required: "bathrooms is required" })} />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-4/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Square Footage
+                      </label>
+                      <input type="number" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="footage" {...register("footage", { required: "square footage is required" })} />
+                    </div>
+                  </div>
+                </div>
+                <hr className="mt-6 border-b-1 border-blueGray-300" />
+                <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                  Property Condition
+                </h6>
+                <div className="flex flex-wrap">
+                  <div className="w-full lg:w-1/2 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Condition
+                      </label>
+                      <select name="condition" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" {...register('condition', { required: "condition is required" })}>
+                        <option defaultValue hidden>Select</option>
+                        <option value="Excellent">Excellent</option>
+                        <option value="Good">Good</option>
+                        <option value="Fair">Fair</option>
+                        <option value="Poor">Poor</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-1/2 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Recent Renovations
+                      </label>
+                      <select name="renovation" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" {...register('renovation', { required: "renovation is required" })}>
+                        <option defaultValue hidden>Select</option>
+                        <option value="Yes">Yes</option>
+                        <option value="No">No</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <hr className="mt-6 border-b-1 border-blueGray-300" />
+                <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                  Comparable Sales
+                </h6>
+                <div className="flex flex-wrap">
+                  <div className="w-full lg:w-1/2 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="firstAddress">
+                        Address 1
+                      </label>
+                      <input type="text" id="firstAddress" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="firstAddress"
+                        {...register("firstAddress", { required: 'First Address is required' })} />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-1/2 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="secondAddress">
+                        Address 2
+                      </label>
+                      <input type="text" id="secondAddress" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="secondAddress"
+                        {...register("secondAddress", {required: 'Secondary Address is required'})} />
+                    </div>
+                  </div>
+                </div>
+                <hr className="mt-6 border-b-1 border-blueGray-300" />
+                <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                  Property Features
+                </h6>
+                <div className="flex flex-wrap">
+                  <div className="w-full px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="features">
+                        Special Features or Upgrades
+                      </label>
+                      <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="features" {...register("features", { required: "features is required" })} />
+                    </div>
+                  </div>
+                </div>
+                <hr className="mt-6 border-b-1 border-blueGray-300" />
+                <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                  Zoning and Land Use
+                </h6>
+                <div className="flex flex-wrap">
+                  <div className="w-full lg:w-1/2 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="condition">
+                        Condition
+                      </label>
+                      <select name="condition" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" {...register('condition', { required: "condition is required" })}>
+                        <option defaultValue hidden>Select</option>
+                        <option value="Residential">Residential</option>
+                        <option value="Commercial">Commercial</option>
+                        <option value="Mixed-Use">Mixed-Use</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-1/2 px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="landUse">
+                        Permitted Land Use
+                      </label>
+                      <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" name="landUse"  {...register("landUse", { required: "landUse is required" })} />
+                    </div>
+                  </div>
+                </div>
+                <hr className="mt-6 border-b-1 border-blueGray-300" />
+                <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                  Purpose
+                </h6>
+                <div className="flex flex-wrap">
+                  <div className="w-full px-4">
+                    <div className="relative w-full mb-3">
+                      <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Purpose
+                      </label>
+                      <select name="condition" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" {...register('condition', { required: "condition is required" })}>
+                        <option defaultValue hidden>Select</option>
+                        <option value="Selling">Selling</option>
+                        <option value="Refinancing">Refinancing</option>
+                        <option value="Insurance">Insurance</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-wrap">
+                  <button type="submit" className="bg-indigo-500 w-full py-2 text-white font-bold">Generate</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-        {/* renovations */}
-        <div>
-          <label htmlFor="location">Recent Renovations</label>
-          <div>
-            <select name="renovation" {...register('renovation')}>
-              <option defaultValue hidden>Select</option>
-              <option value="Yes">Yes</option>
-              <option value="No">No</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      {/* comparable sales */}
-      <div>
-        <label htmlFor="location">Comparable Sales</label>
-        <div>
-          <input type="text" name="recentSales" placeholder="Recent Sales" {...register('recentSales')} />
-        </div>
-      </div>
-      {/* property features */}
-      <div>
-        <label htmlFor="location">Special Features</label>
-        <div>
-          <input type="text" name="recentSales" placeholder="Recent Sales" {...register('recentSales')} />
-        </div>
-      </div>
-      {/* Zoning and Land Use */}
-      <div>
-        <label htmlFor="zoning">Zoning and Land Use</label>
-        <div>
-          <select name="zoning" {...register('zoning')}>
-            <option defaultValue hidden>Select</option>
-            <option value="Excellent">Residential</option>
-            <option value="Good">Commercial</option>
-            <option value="Fair">Mixed-Use</option>
-          </select>
-        </div>
-      </div>
-      {/* Purpose of Valuation */}
-      <div>
-        <label htmlFor="purpose">Purpose of Valuation</label>
-        <div>
-          <select name="purpose" {...register('purpose')}>
-            <option defaultValue hidden>Select</option>
-            <option value="Excellent">Selling</option>
-            <option value="Good">Refinancing</option>
-            <option value="Fair">Insurance</option>
-          </select>
-        </div>
-      </div>
-      {/* submit */}
-      <button className="px-4 py-2 my-4 bg-slate-900 text-white" type="submit">Generate Valuation</button>
-    </form>
-  )
+      </section>
+    </div>
+  );
 }
--- src/layout/dasboardLayout/DashboardLayout.jsx
+++ src/layout/dasboardLayout/DashboardLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function DashboardLayout() {
   return (
-    <section>
+    <section className="container mx-auto">
       <Navbar />
       <Outlet/>
     </section>
