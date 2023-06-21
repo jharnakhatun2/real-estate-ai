f13177461592a41291dfc1d1db1caa0e5f40b077
codernoyan <coder.noyan@gmail.com>
2023-06-21 22:04:46 +0600

    form design changed

--- src/components/dashboard/propertyForm/Form.jsx
+++ src/components/dashboard/propertyForm/Form.jsx
@@ -13,7 +13,7 @@ export default function Form() {
   
   return (
     <div>
-      <section className="py-1 bg-indigo-50/20">
+      <section className="py-1 shadow-sm">
         <div className="w-full px-4 mx-auto mt-6">
           <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
             <div className="rounded-t bg-white mb-0 px-6 py-6">
@@ -23,7 +23,7 @@ export default function Form() {
                 </h6>
               </div>
             </div>
-            <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+            <div className="flex-auto px-4 lg:px-10 py-10 pt-0 bg-indigo-50/50">
               <form onSubmit={handleSubmit(onSubmit)}>
                 <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
                   Location Information
