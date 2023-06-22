da09fe387367574a7824d9425d3eb2009c1c6a77
Jharna203 <jharnakhatun203@gmail.com>
2023-06-22 13:57:44 +0600

    added authentication

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <Link to="/signIn">Form</Link>
+      <Link to="/register">Form</Link>
     </>
   );
 }
--- src/pages/SignUP/Login.jsx
+++ src/pages/SignUP/Login.jsx
@@ -1,4 +1,12 @@
+import { useForm } from "react-hook-form";
+
+
 export default function Login() {
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const handleLogin = data => {
+    console.log(data);
+  }
+
   return (
     <div className="bg-[#f8f9fa] py-10">
       {" "}
@@ -14,7 +22,7 @@ export default function Login() {
               {" "}
               <div className="flex items-center justify-center space-x-4 mt-3">
                 {" "}
-                <button className="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-indigo-500 border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                <button className="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-[#7C6EE4] border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
                   {" "}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -29,7 +37,7 @@ export default function Login() {
                   </svg>{" "}
                   Github{" "}
                 </button>{" "}
-                <button className="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-indigo-500 border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                <button className="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-[#7C6EE4] border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
                   {" "}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -65,64 +73,74 @@ export default function Login() {
               {" "}
               Or sign in with credentials{" "}
             </p>{" "}
-            <form className="mt-6">
+
+            {/* Login Form */}
+            <form onSubmit={handleSubmit(handleLogin)} className="mt-6">
               {" "}
-              <div className="relative">
-                {" "}
-                <input
-                  className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
-                  id="username"
-                  type="text"
-                  placeholder="Email"
-                />{" "}
-                <div className="absolute left-0 inset-y-0 flex items-center">
-                  {" "}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-7 w-7 ml-3 text-gray-400 p-1"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    {" "}
-                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />{" "}
-                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />{" "}
-                  </svg>{" "}
-                </div>{" "}
-              </div>{" "}
+              {/* Email Input Field */}
               <div className="relative mt-3">
-                {" "}
-                <input
-                  className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
-                  id="username"
-                  type="text"
-                  placeholder="Password"
-                />{" "}
-                <div className="absolute left-0 inset-y-0 flex items-center">
                   {" "}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-7 w-7 ml-3 text-gray-400 p-1"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
+                  <input
+                    {...register("email", { required: "Email is required !" })}
+                    className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
+                    id="email"
+                    type="email"
+                    placeholder="Email"
+                  />{" "}
+                  <div className="absolute left-0 inset-y-0 flex items-center">
                     {" "}
-                    <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />{" "}
-                  </svg>{" "}
-                </div>{" "}
-              </div>{" "}
-              <div className="mt-4 flex items-center text-gray-500">
-                {" "}
-                <input
-                  type="checkbox"
-                  id="remember"
-                  name="remember"
-                  className="mr-3"
-                />{" "}
-                <label htmlFor="remember">Remember me</label>{" "}
-              </div>{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-7 w-7 ml-3 text-gray-400 p-1"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      {" "}
+                      <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />{" "}
+                      <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />{" "}
+                    </svg>{" "}
+                  </div>{" "}
+                </div>
+                {errors.email && (
+                  <p className="text-danger" role="alert">
+                    {errors.email?.message}
+                  </p>
+                )}{" "}
+              {/* Password Input Field */}
+             <div className="relative mt-3">
+                  {" "}
+                  <input
+                    {...register("password", {
+                      required: "Password is required !"
+                    })}
+                    className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
+                    id="password"
+                    type="password"
+                    placeholder="Password"
+                  />{" "}
+                  <div className="absolute left-0 inset-y-0 flex items-center">
+                    {" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-7 w-7 ml-3 text-gray-400 p-1"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      {" "}
+                      <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />{" "}
+                    </svg>{" "}
+                  </div>{" "}
+                </div>
+                {errors.password && (
+                  <p className="text-danger" role="alert">
+                    {errors.password?.message}
+                  </p>
+                )}{" "}
+            
+              {/* Form Submit  */}
               <div className="flex items-center justify-center mt-8">
                 {" "}
-                <button className="text-white py-2 px-4 uppercase rounded bg-indigo-500 hover:bg-indigo-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                <button type="submit" className="text-white py-2 px-4 uppercase rounded bg-[#9f95e9] hover:bg-[#7C6EE4] shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
                   {" "}
                   Sign in{" "}
                 </button>{" "}
