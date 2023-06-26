e70d7de73a10eddf751a85631addf0684cf91e28
codernoyan <coder.noyan@gmail.com>
2023-06-26 14:07:25 +0600

    loading bugs fixed

--- src/context/authProvider/AuthProvider.jsx
+++ src/context/authProvider/AuthProvider.jsx
@@ -9,26 +9,31 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState();
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     };
 
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password)
     };
 
     const updateUser = (userInfo) => {
+        setLoading(true);
         return updateProfile(auth.currentUser, userInfo);
     };
 
     const providerLogin = (provider) => {
+        setLoading(true);
         return signInWithPopup(auth, provider);
     };
 
     //Log Out 
     const logOut = () => {
+        setLoading(true);
         toast.success("Logout Successful");
         return signOut(auth);
     };
@@ -38,6 +43,7 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             console.log('user observing');
             setUser(currentUser);
+            setLoading(false);
         })
         return () => unsubscribe();
     }, []);
--- src/ui/loading/Loading.jsx
+++ src/ui/loading/Loading.jsx
@@ -1,7 +1,13 @@
+import buildingLoading from 'assets/Animation/building-loading.json';
+import Lottie from "lottie-react";
+
 export default function Loading() {
   return (
-    <section>
-      <h2>Loading...</h2>
+    <section className="flex justify-center items-center h-screen">
+      <div className="lg:w-1/2">
+        {/* lottie */}
+        <Lottie className="aspect-square lg:aspect-auto p-40 lg:mt-0" animationData={buildingLoading} loop={true} />
+      </div>
     </section>
   )
 }
