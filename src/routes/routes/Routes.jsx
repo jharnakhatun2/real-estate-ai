76b98d7a8ba0980cddad175e81069af362e7eb0d
codernoyan <coder.noyan@gmail.com>
2023-06-26 18:42:41 +0600

    lazy comopnent removed

--- src/routes/routes/Routes.jsx
+++ src/routes/routes/Routes.jsx
@@ -5,22 +5,15 @@ import Properties from "pages/properties/Properties";
 import Property from "pages/properties/Property";
 import Login from "pages/signUP/Login";
 import Register from "pages/signUP/Register";
-import { Suspense, lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import PrivateRoute from "routes/privateRoute/PrivateRoute";
-import Loading from "ui/loading/Loading";
+import Main from "../../layout/Main";
 import Home from "../../pages/home/Home";
-const LazyMain = lazy(() => import("../../layout/Main"));
-
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <Suspense fallback={<Loading/>}>
-        <LazyMain />
-      </Suspense>
-    ),
+    element: <Main />,
     errorElement: <NotFound />,
     children: [
       {
