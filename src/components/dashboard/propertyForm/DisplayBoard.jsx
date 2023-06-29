fddd7bd5f1f5e77c3f92e7c1b6e0fa88bc20fe69
codernoyan <coder.noyan@gmail.com>
2023-06-30 03:55:33 +0600

    bugs fixed

--- src/components/dashboard/propertyForm/DisplayBoard.jsx
+++ src/components/dashboard/propertyForm/DisplayBoard.jsx
@@ -4,27 +4,34 @@ import Lottie from "lottie-react";
 export default function DisplayBoard({ propertyData, loading }) {
   const { createdText, imageUrl } = propertyData || {};
   // splited texxt
-  // const splitedText = createdText.split(",")
+  // const splitedText = createdText?.split(". ");
   return (
-    <>
-      <div className="p-4">
+    <div className={!createdText && !loading && "flex justify-center items-center h-screen border-dashed border-2 m-4 border-slate-300"}>
+      <div className="p-8">
         <div>
           {
-            !loading ?
-              <div className="">
+            loading ?
+              <div>
                 <Lottie
                   className="aspect-square w-full p-4 -mt-10"
                   animationData={imageLoading}
                   loop={true} />
               </div>
               :
-              <img className="w-full aspect-square" src={imageUrl ? imageUrl : "https://namcc.org.za/img/business.jpg"} alt="property" />
+              <img className={`w-full aspect-square ${imageUrl || "hidden"} rounded-md shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]`} src={imageUrl} alt="property" />
           }
         </div>
+        {
+          !createdText && !loading && (
+            <div>
+              <h2 className="text-center font-bold text-6xl text-slate-400">Generate Now</h2>
+            </div>
+          )
+        }
         <div>
           {
-            !loading ?
-                (
+            loading ?
+              (
                 <div className="w-full max-w-full mx-auto animate-pulse p-2 pb-4 -mt-20">
                   <h1 className="h-4 bg-gray-500 rounded-lg w-3/4"></h1>
                   <p className="w-5/6 h-4 mt-6 bg-gray-400/80 rounded-lg"></p>
@@ -36,12 +43,18 @@ export default function DisplayBoard({ propertyData, loading }) {
                   <p className="w-11/12 h-4 mt-4 bg-gray-400/80 rounded-lg"></p>
                   <p className="w-full h-4 mt-4 bg-gray-400/80 rounded-lg"></p>
                 </div>
-                )
+              )
               :
-              <p>{createdText}</p>
+              <p className="mt-4 text-justify">{createdText}</p>
+            
           }
+          {/* {
+            splitedText?.map((text, i) => (
+              <p key={i} className="text-sm text-justify">{text}</p>
+            ))
+          } */}
         </div>
       </div>
-    </>
+    </div>
   )
 }
