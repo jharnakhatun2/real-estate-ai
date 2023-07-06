436d9f37b5d54b658c8950f259783d4aced86213
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-07-07 02:11:57 +0600

    added a modal commit

--- src/pages/properties/Properties.jsx
+++ src/pages/properties/Properties.jsx
@@ -40,6 +40,7 @@ export default function Properties() {
             setPropertyData={setPropertyData}
             ></PropertiesCard>)}
         </div>
+        {/* property Valuation modal */}
         {
           propertyData &&
           <DisplayModal 
